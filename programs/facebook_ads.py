0f24ff404604515338580523b16d70ee0b86afd6
SabK <sabbe.kev@gmail.com>
2019-05-06 14:48:08 +0700

    ADD google_ads.py & lib.py

--- programs/facebook_ads.py
+++ programs/facebook_ads.py
@@ -6,36 +6,7 @@ from facebook_business.adobjects.ad import Ad
 import json
 import sys
 
-class File:
-    def write(self, filename, content):
-        file = open(filename, "w")
-        file.write(content + "\n")
-        file.close()
-
-    def append(self, filename, content):
-        file = open(filename, "a")
-        file.write(content.encode("utf-8") + "\n")
-        file.close()
-
-    def readLines(self, filename):
-        file = open(filename, 'r')
-        lines = file.readlines()
-        file.close()
-        return lines
-
-    def read(self, filename):
-        file = open(filename, 'r')
-        content = file.read()
-        file.close()
-        return content
-
-    def toArray(self, filename):
-        array = []
-        for line in self.readLines(filename):
-            formattedLine = line.rstrip()
-            if formattedLine != '':
-                array.append(formattedLine)
-        return array
+from lib import File
 
 File = File()
 
